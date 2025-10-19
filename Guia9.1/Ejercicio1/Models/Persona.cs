58834428837c81e9802bde30bd599c8b20f36c8e
alevittHome <alejandrovitt@hotmail.com>
2025-10-19 19:29:05 -0300

    inicio del repo

---
+++ Guia9.1/Ejercicio1/Models/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    internal class Persona:IComparable<Persona>
+    {
+        string nombre;
+        int dni;
+
+        public string Nombre { get => nombre; private set => nombre = value; }
+        public int Dni { get => dni; private set => dni = value; }
+
+        public Persona() { }
+        public Persona(string nombre, int dni)
+        {
+            Nombre = nombre;
+            Dni = dni;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nombre} (DNI: {Dni})";
+        }
+
+        public int Compareto(Persona other)
+        {
+            if (other == null) return -1;
+            return this.Dni.CompareTo(other.Dni);
+        }
+
+    }
+}
